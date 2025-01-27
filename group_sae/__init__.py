d70c800524542aaf6a3a91a2f4638ed6db2c94a9
ghidav <davide.ghilardi0@gmail.com>
2025-01-27 14:58:44 +0000

    Feature Dataset

--- group_sae/__init__.py
+++ group_sae/__init__.py
@@ -2,5 +2,6 @@ from .config import RunConfig, SaeConfig, TrainConfig
 from .sae import Sae
 from .trainer import SaeTrainer
 from .trainer_cluster import ClusterSaeTrainer
+from .utils import CLUSTER_MAP
 
 __all__ = ["Sae", "SaeConfig", "ClusterSaeTrainer", "RunConfig", "SaeTrainer", "TrainConfig"]
