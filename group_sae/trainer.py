Fix activation scaling

group_sae/trainer.py
@@ -83,7 +83,6 @@ class SaeTrainer:
         # SAEs
         self.model = model
         device = model.device
-        print(f"Input shapes: {input_shapes}")
         self.saes = {
             hook: Sae(input_shapes[hook][-1], cfg.sae, device) for hook in self.local_hookpoints()
         }
@@ -303,7 +302,8 @@ class SaeTrainer:
         maybe_wrapped: dict[str, DDP] | dict[str, Sae] = {}
         module_to_name = {v: k for k, v in name_to_module.items()}
 
-        for batch_idx, batch in enumerate(self.dataloader):
+        batch_idx = 0
+        for batch in self.dataloader:
             if self.global_step >= self.training_steps:
                 break
 
@@ -350,6 +350,9 @@ class SaeTrainer:
                     running_mean_act_norm[name] = update_running_mean(
                         running_mean_act_norm[name], l2_norm, batch_idx + 1
                     )
+                running_mean_act_norm[name] = float(
+                    self.maybe_all_reduce(running_mean_act_norm[name], "mean")
+                )
 
             for name, hiddens in hidden_dict.items():
                 raw = self.saes[name]  # 'raw' never has a DDP wrapper
@@ -380,24 +383,6 @@ class SaeTrainer:
                 # Make sure the W_dec is still unit-norm
                 if raw.cfg.normalize_decoder:
                     raw.set_decoder_norm_to_unit_norm()
-
-                # Normalize the activations
-                if self.cfg.normalize_activations:
-                    with torch.no_grad():
-                        hiddens = hiddens * self.scaling_factors[name]
-
-                # Save the running mean of the L2 norm of the activations
-                l2_norm = hiddens.norm(p=2, dim=-1).mean()
-                if name not in running_mean_act_norm:
-                    running_mean_act_norm[name] = l2_norm
-                else:
-                    running_mean_act_norm[name] = update_running_mean(
-                        running_mean_act_norm[name], l2_norm, batch_idx + 1
-                    )
-                running_mean_act_norm[name] = float(
-                    self.maybe_all_reduce(running_mean_act_norm[name], "mean")
-                )
-                avg_act_norm[name] = float(self.maybe_all_reduce(l2_norm, "mean"))
 
                 acc_steps = self.cfg.grad_acc_steps * self.cfg.micro_acc_steps
                 denom = acc_steps * self.cfg.wandb_log_frequency
@@ -550,6 +535,7 @@ class SaeTrainer:
                 if (step + 1) % self.cfg.save_every == 0:
                     self.save()
 
+            batch_idx += 1
             self.global_step += 1
             pbar.update()
 