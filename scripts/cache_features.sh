d337148b5051532c353e8812cc5964c1110e7647
Marco <marcomolinari4@gmail.com>
2025-02-10 22:11:51 +0000

    scripts for latents

--- scripts/cache_features.sh
+++ scripts/cache_features.sh
@@ -17,8 +17,6 @@ python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_0
 python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 8
 python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 9
 python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 10
-python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 11
-python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 12
 
 python3.11 interp/cache_activations.py --model_name pythia-1b --n_tokens 100_000 --batch_size 8 --ctx_len 512
 python3.11 interp/cache_activations.py --model_name pythia-1b --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 1
--- scripts/gpu1.sh
+++ scripts/gpu1.sh
@@ -1,4 +1,5 @@
-python3.11 interp/cache_activations.py --model_name pythia-160m --n_tokens 100_000 --n_splits 5 --batch_size 8 --ctx_len 512 --cluster --G 2
-python3.11 interp/cache_activations.py --model_name pythia-160m --n_tokens 100_000 --n_splits 5 --batch_size 8 --ctx_len 512 --cluster --G 3
-python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_000 --n_splits 5 --batch_size 8 --ctx_len 512 --cluster --G 1
-python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_000 --n_splits 5 --batch_size 8 --ctx_len 512 --cluster --G 2
+python3.11 interp/cache_activations.py --model_name pythia-160m --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 4
+python3.11 interp/cache_activations.py --model_name pythia-160m --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 5
+python3.11 interp/cache_activations.py --model_name pythia-160m --n_tokens 100_000 --batch_size 8 --ctx_len 512 --cluster --G 6
+
+python3.11 interp/cache_activations.py --model_name pythia-410m --n_tokens 100_000 --batch_size 8 --ctx_len 512
