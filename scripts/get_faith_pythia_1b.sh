a3bbe2004ca7b5df78feb37234550bbe1356eb0e
belerico <belo.fede@outlook.com>
2025-02-11 17:03:41 +0000

    Add scripts pythia-1b

---
+++ scripts/get_faith_pythia_1b.sh
@@ -0,0 +1,55 @@
+for W in faithfulness completeness; do
+    for K in -1 1 2 3 4 5 6 7 8 9 10 11 12 13 14; do
+        python ./downstream/faith_topk.py \
+        -d ioi \
+        -c resid_post \
+        -n 1024 \
+        -mt attrib \
+        --model pythia-1b \
+        -w faithfulness \
+        --sae_root_folder /home/fbelotti/group-sae/saes/pythia_1b-topk \
+        --effects_dir /home/fbelotti/group-sae/effects \
+        --task_dir /home/fbelotti/group-sae/downstream/tasks \
+        --faith_dir faithfulness_topk \
+        --n_devices 2 \
+        --batch_size 512 \
+        --what $W \
+        --K $K
+    done
+
+    for K in -1 1 2 3 4 5 6 7 8 9 10 11 12 13 14; do
+        python ./downstream/faith_topk.py \
+        -d subject_verb \
+        -c resid_post \
+        -n 1024 \
+        -mt attrib \
+        --model pythia-1b \
+        -w faithfulness \
+        --sae_root_folder /home/fbelotti/group-sae/saes/pythia_1b-topk \
+        --effects_dir /home/fbelotti/group-sae/effects \
+        --task_dir /home/fbelotti/group-sae/downstream/tasks \
+        --faith_dir faithfulness_topk \
+        --n_devices 2 \
+        --batch_size 512 \
+        --what $W \
+        --K $K
+    done
+
+    for K in -1 1 2 3 4 5 6 7 8 9 10 11 12 13 14; do
+        python ./downstream/faith_topk.py \
+        -d greater_than \
+        -c resid_post \
+        -n 1024 \
+        -mt attrib \
+        --model pythia-1b \
+        -w faithfulness \
+        --sae_root_folder /home/fbelotti/group-sae/saes/pythia_1b-topk \
+        --effects_dir /home/fbelotti/group-sae/effects \
+        --task_dir /home/fbelotti/group-sae/downstream/tasks \
+        --faith_dir faithfulness_topk \
+        --n_devices 2 \
+        --batch_size 512 \
+        --what $W \
+        --K $K
+    done
+done
