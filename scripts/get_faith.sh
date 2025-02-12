9932ee4592dff152d32907c3fe591ea06d2bd4ca
Marco <marcomolinari4@gmail.com>
2025-02-12 01:43:21 +0000

    all faith and completeness

---
+++ scripts/get_faith.sh
@@ -0,0 +1,5 @@
+python3.11 ./downstream/faith_topk.py \
+    -d ioi \
+    --model pythia-1b \
+    --what faithfulness \
+    --K 4
--- scripts/gpu3.sh
+++ scripts/gpu3.sh
@@ -1,7 +1,15 @@
-for K in -1 1 2 3 4 5 6 7 8 9 10 11 12 13 14; do
-    python3.11 downstream/effects.py \
+for K in -1 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22; do
+    python3.11 ./downstream/faith_topk.py \
     -d ioi \
-    -l 15 \
-    -m pythia-1b \
+    --model pythia-410m \
+    --what completeness \
+    --K $K
+done
+
+for K in -1 1 2 3 4 5 6 7 8 9 10 11 12 13 14; do
+    python3.11 ./downstream/faith_topk.py \
+    -d ioi \
+    --model pythia-1b \
+    --what completeness \
     --K $K
 done
