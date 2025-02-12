20831ad68d85fda0f1be07eb69bc6e33b9652ddf
belerico <belo.fede@outlook.com>
2025-02-12 13:39:53 +0000

    Update script

--- scripts/cache_features.sh
+++ scripts/cache_features.sh
@@ -1,29 +1,8 @@
-python3.11 interp/cache_activations.py --model_name pythia-160m 
-python3.11 interp/cache_activations.py --model_name pythia-160m --cluster --G 1
-python3.11 interp/cache_activations.py --model_name pythia-160m --cluster --G 2
-python3.11 interp/cache_activations.py --model_name pythia-160m --cluster --G 3
-python3.11 interp/cache_activations.py --model_name pythia-160m --cluster --G 4
-python3.11 interp/cache_activations.py --model_name pythia-160m --cluster --G 5
-python3.11 interp/cache_activations.py --model_name pythia-160m --cluster --G 6
+python3.11 interp/cache_activations.py --model_name pythia-160m
+python3.11 interp/cache_activations.py --model_name pythia-160m --cluster
 
-python3.11 interp/cache_activations.py --model_name pythia-410m 
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 1
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 2
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 3
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 4
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 5
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 6
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 7
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 8
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 9
-python3.11 interp/cache_activations.py --model_name pythia-410m --cluster --G 10
+python3.11 interp/cache_activations.py --model_name pythia-410m
+python3.11 interp/cache_activations.py --model_name pythia-410m --cluster
 
-python3.11 interp/cache_activations.py --model_name pythia-1b 
-python3.11 interp/cache_activations.py --model_name pythia-1b --cluster --G 1
-python3.11 interp/cache_activations.py --model_name pythia-1b --cluster --G 2
-python3.11 interp/cache_activations.py --model_name pythia-1b --cluster --G 3
-python3.11 interp/cache_activations.py --model_name pythia-1b --cluster --G 4
-python3.11 interp/cache_activations.py --model_name pythia-1b --cluster --G 5
-python3.11 interp/cache_activations.py --model_name pythia-1b --cluster --G 6
-python3.11 interp/cache_activations.py --model_name pythia-1b --cluster --G 7
-python3.11 interp/cache_activations.py --model_name pythia-1b --cluster --G 8
+python3.11 interp/cache_activations.py --model_name pythia-1b
+python3.11 interp/cache_activations.py --model_name pythia-1b --cluster
